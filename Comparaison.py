e7ac80c51a58b85934987e95c1024560bed32d31
anselmeouedraogo833-ai <anselmeouedraogo833@gmail.com>
2025-09-22 17:33:08 +0000

    Update Comparaison.py

--- Comparaison.py
+++ Comparaison.py
@@ -1,20 +1,23 @@
 import math  # pour utiliser sqrt (racine carrée)
 
 while True:
-    print("Welcome! Here you can compare two numbers y compris les racines carrées ")
+      print("I am Anselme ,a code's learner")
+      print("Welcome! Here you can compare two numbers even math.sqrt( racine carrée ")
+      print("for number  (√x) , put math.sqrt(x) ou x**0.5")
+      a = eval(input("Entrez la valeur de a : "))
+      b = eval(input("Entrez la valeur de b : "))
 
-    a = eval(input("Entrez la valeur de a : "))
-    b = eval(input("Entrez la valeur de b : "))
+if a == b:
+   print("a est égal à b")
+elif a < b:
+     print("a est inférieur à b")
+else:
+     print("a est supérieur à b")
 
-    if a == b:
-        print("a est égal à b")
-    elif a < b:
-        print("a est inférieur à b")
-    else:
-        print("a est supérieur à b")
-
-    reponse = input("Veux-tu recommencer ? (oui/non) : ").lower()
-    if reponse != "oui":
+     reponse = input("Fais decouvrir mon code à  tes amis Veux-tu recommencer ? (oui/non) : ").lower()
+if reponse != "oui":
         print("Merci d'avoir utilisé mon programme !")
-        break
+        break 
+
+
       
