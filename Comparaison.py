372e5da8d76266f4c6242559739df76ca44939b8
anselmeouedraogo833-ai <anselmeouedraogo833@gmail.com>
2025-09-22 17:59:01 +0000

    Update Comparaison.py
    Compare your math's numbers

--- Comparaison.py
+++ Comparaison.py
@@ -1,23 +1,23 @@
-import math  # pour utiliser sqrt (racine carrée)
+import math
 
 while True:
-      print("I am Anselme ,a code's learner")
-      print("Welcome! Here you can compare two numbers even math.sqrt( racine carrée ")
-      print("for number  (√x) , put math.sqrt(x) ou x**0.5")
-      a = eval(input("Entrez la valeur de a : "))
-      b = eval(input("Entrez la valeur de b : "))
+    print("I am Anselme, a code's learner")
+    print("Welcome! Here you can compare two numbers even with math.sqrt (√)")
+    print("For square root, enter math.sqrt(x) or x**0.5")
 
-if a == b:
-   print("a est égal à b")
-elif a < b:
-     print("a est inférieur à b")
-else:
-     print("a est supérieur à b")
+    a = eval(input("Entrez la valeur de a : "))
+    b = eval(input("Entrez la valeur de b : "))
 
-       reponse = input("Share my code to your friends. Do you want to continue ? (Yes/no) : ").lower()
-    if reponse != "oui":
-        print("Thanks your using my code  !")
+    if a == b:
+        print("a est égal à b")
+    elif a < b:
+        print("a est inférieur à b")
+    else:
+        print("a est supérieur à b")
+
+    reponse = input("Share my code to your friends. Do you want to continue? (Yes/no) : ").lower()
+    if reponse != "yes":
+        print("Thanks for using my code!")
         break
-          
 
-      
+
